dc73e34d953a168e009c6c1e16aa4d097bf699b2
Chanakan393 <chana.sch17@gmail.com>
2026-02-20 14:44:05 +0700

    เปลี่ยน localhost

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000; 
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
